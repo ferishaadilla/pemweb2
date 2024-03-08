ca5f1eb7c7096a94740aa5937c1e783b4b21463a
unknown <ferishaadilla@gmail.com>
2024-03-08 10:38:42 +0700

    praktikum-3

---
+++ praktik3/proses_regristrasi.php
@@ -0,0 +1,10 @@
+<?php
+$domisili = ["Jakarta", "Bogor", "Depok", "Tanggerang", "Bekasi"];
+
+$program_studi = [
+    "SI" => "Sistem Informasi",
+    "TI" => "Teknik Iformatika",
+    "BD" => "Bisnis Digital"
+];
+?>
+
